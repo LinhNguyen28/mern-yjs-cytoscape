7a24974e03da7825456455b39fe84f298150e430
Linh Nguyen <nguyen44@miamioh.edu>
2021-10-04 06:58:03 -0400

    sync changes complete

--- frontend/src/components/playground.js
+++ frontend/src/components/playground.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect, Component } from "react";
+import React, { Component } from "react";
 import * as Y from "yjs";
 import { WebsocketProvider } from "y-websocket";
-// import { IndexeddbPersistence } from "y-indexeddb";
 import CytoscapeComponent from 'react-cytoscapejs';
 
 export default class Playground extends Component {
@@ -12,92 +11,85 @@ export default class Playground extends Component {
       this.ydoc
    );
    elementsArr = this.ydoc.getArray("map");
-   initialE = [
-      { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 50 } },
-      { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 50 } },
-      { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
-   ];
 
    state = {
       w: 0,
       h: 0,
-      elements: this.elementsArr.toArray(),
-      nodeId: 3,
+      elements: [],
+      nodeId: 1,
       textContent: "Disconnect",
    }
 
    setUpListeners = () => {
       this.cy.on('click', (event) => {
          if (event.target === this.cy) {
-            // this.cy.add({
-            //    data: {
-            //       id: `${this.state.nodeId}`,
-            //       label: `Node ${this.state.nodeId}`
-            //    },
-            //    position: { x: event.position.x, y: event.position.y }
-            // });
-            
-            this.elementsArr.push([{
+            this.cy.add({
                data: {
                   id: `${this.state.nodeId}`,
                   label: `Node ${this.state.nodeId}`
                },
                position: { x: event.position.x, y: event.position.y }
-            }]);
-            console.log(this.elementsArr.toJSON());
-            if (this.elementsArr.length > 10) {
-               console.log("Need to delete some nodes");
-               this.elementsArr.delete(0, this.elementsArr.length);
-               this.elementsArr.insert(0, this.initialE);
-            }
-            this.setState({
-               nodeId: this.state.nodeId + 1,
-            })
+            });
          }
-      })
+         this.updateElementsArr();
+      });
+
+      this.cy.on("drag", "node", event => {
+         console.log(event);
+         console.log("dragfree at");
+         console.log(event.position);
+      });
    }
 
    componentDidMount = () => {
+      console.log("componentDidMount");
       this.setState({
          w: window.innerWidth - 50,
          h: window.innerHeight - 50,
       });
       this.setUpListeners();
-      this.provider.connect();
+      this.connect();
       this.elementsArr.observe((event, transaction) => {
-         console.log("Sync changes");
+         console.log("sync changes");
          this.setState({
             elements: this.elementsArr.toArray(),
+            nodeId: this.elementsArr.length + 1,
          })
       });
    }
 
+   updateElementsArr = () => {
+      let elements = this.cy.json(true)["elements"];
+      console.log(elements);
+      this.elementsArr.delete(0, this.elementsArr.length);
+      this.elementsArr.insert(0, elements);
+   }
+
    connect = () => {
-      if (this.provider.shouldConnect) {
-         console.log("Here")
+      if (this.provider.wsconnected || this.provider.wsconnecting) {
          this.provider.disconnect();
-         this.elementsArr.unobserve(() => {
-            console.log("No more observation or sync")
-         });
          this.setState({textContent:"Connect"});
       } else {
          this.provider.connect();
-         this.elementsArr.observe((event, transaction) => {
-            console.log("Sync changes");
-            this.setState({
-               elements: this.elementsArr.toArray(),
-            })
-         });
          this.setState({textContent:"Disconnect"});
       }
+   }
+
+   clear = () => {
+      this.elementsArr.delete(0, this.elementsArr.length);
    }
 
    render() {
       return (<div>
          <button type="button" 
-         id="y-connect-btn" 
-         onClick={this.connect}>
-            {this.state.textContent}
+            id="y-connect-btn" 
+            onClick={this.connect}>
+               {this.state.textContent}
+         </button>
+         <button type="button" 
+            id="clear-btn" 
+            onClick={this.clear}>
+               Clear
          </button>
          <CytoscapeComponent
             elements={this.state.elements}
